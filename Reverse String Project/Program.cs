a36e947177d7f80d216a9d855c8a94531fae9516
1nature <olanipekun1439@yahoo.com>
2023-07-26 18:06:17 +0100

    Add project files.

---
+++ Reverse String Project/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Reverse_String_Project
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)  
+        {
+            Console.WriteLine("Please input any string name to check whether it is a palindrome");
+            
+            string userInput = Console.ReadLine();
+            string reverseString = string.Empty;
+            
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                reverseString = reverseString + userInput[i]; 
+            }
+                
+            Console.WriteLine(reverseString);
+            if (reverseString == userInput)
+            {
+                Console.WriteLine($"{userInput} is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is not a palindrome");
+            }
+
+        }
+    }
+}
